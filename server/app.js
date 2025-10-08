764a39ca251eee2c44d209e0e4e4228813f9152c
bitaniyadamtew <bitaniyadamtew@gmail.com>
2025-10-08 18:44:44 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,15 +1,1 @@
-require(dotenv).config()
 
-const TelegramBot=require('../server/bot.js')
-
-const express=require('express')
-
-const app=express()
-
-app.use(express.json())
-
-// const port=process.env.port
-
-// app.listen(port,()=>{
-//     console.log(`Server starting on ${port}`)
-// })  a telegram bot
